26853da1dd3e1f6c62a8c4a49a076236e42a43a1
iuryfranca <iurygfranca@gmail.com>
2023-07-05 23:44:50 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hai-bosu',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
